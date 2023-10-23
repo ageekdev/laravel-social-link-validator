f1f6320aa260ee23a75fc6f358c36e639ebdb3b2
yethusoe91 <>
2023-10-23 09:50:56 +0630

    update testcase

--- tests/Datasets/Platforms.php
+++ tests/Datasets/Platforms.php
@@ -2,8 +2,10 @@
 
 dataset('platforms', [
     ['facebook', 'https://facebook.com/ageekdev'],
-    ['twitter', 'https://twitter.com/ageekdev'],
+    ['instagram', 'https://www.instagram.com/laravelnews'],
     ['line', 'https://line.me/R/ti/p/~ageekdev'],
+    ['linkedin', 'https://www.linkedin.com/company/laravel-news'],
+    ['twitter', 'https://twitter.com/ageekdev'],
     ['whatsapp', 'https://wa.me/1/+660838128312'],
     ['youtube', 'https://youtube.com/channel/gkucmierz'],
 ]);
